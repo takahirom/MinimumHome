871e4f46766793879cca5a9f86eb8b736255a6b4
takahirom <takam.dev@gmail.com>
2014-09-24 00:27:44 +0900

    first commit

---
+++ app/src/main/java/com/kogitune/minimumhome/Home.java
@@ -0,0 +1,12 @@
+package com.kogitune.minimumhome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity {
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+    }
+}
